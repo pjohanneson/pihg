1af1e2e73f0e1603dd8e9f891b5749aded51aad3
pjohanneson <pjohanneson@Lappy>
2013-12-08 15:16:40 -0600

    Repeatable group! (hopefully)

--- lib/cmb-extended/pihg-metaboxen.php
+++ lib/cmb-extended/pihg-metaboxen.php
@@ -37,10 +37,10 @@ function pihg_metaboxen( array $meta_boxes ) {
 			array(
 				'name'	=> 'Seed Info',
 				'id'	=> $prefix . 'seed_info',
-				'type'	=> 'text',
-				'repeatable' => true,
+				'type'	=> 'group',
+				//'repeatable' => true,
 				'desc'	=> 'Seed Info',
-				// 'fields'	=> $seed_info_fields,
+				'fields'	=> $seed_info_fields,
 			),
 		),
 	);
