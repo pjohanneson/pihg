eb4eb59c354ed925645c36486b710412a042c298
pjohanneson <pjohanneson@Lappy>
2013-12-08 14:27:13 -0600

    New CMB library -- testing

--- pihg-core.php
+++ pihg-core.php
@@ -7,7 +7,7 @@
  * Description: Provides CPTs and other enhancements for the PIHG site.
  */
 
-require( plugin_dir_path( __FILE__ ) ) . 'lib/cmb/pihg-metaboxen.php';
+require( plugin_dir_path( __FILE__ ) ) . 'lib/cmb-extended/pihg-metaboxen.php';
 
 class PIHG {
 
