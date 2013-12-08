0ac5979c5f090f8b9fabc5875ae4a2fad18f6c4f
pjohanneson <pjohanneson@Lappy>
2013-12-07 21:45:53 -0600

    Added the JS file to the repo

---
+++ scripts/pihg.jquery.js
@@ -0,0 +1,16 @@
+(function($) {
+	$("a.add_acronym").click( function() {
+		$('table#seed-info > tbody:last').append("\
+<tr>\n\
+	<td><input type='text' name='_pihg_seed_info[year]' /></td>\n\
+	<td><input type='text' name='_pihg_seed_info[PA]' /></td>\n\
+	<td><input type='text' name='_pihg_seed_info[SA]' /></td>\n\
+	<td><input type='text' name='_pihg_seed_info[0A]' /></td>\n\
+	<td><input type='text' name='_pihg_seed_info[LA]' /></td>\n\
+	<td><input type='text' name='_pihg_seed_info[GLA]' /></td>\n\
+	<td><input type='text' name='_pihg_seed_info[ALA]' /></td>\n\
+	<td><input type='text' name='_pihg_seed_info[SDA]' /></td>\n\
+	<td><input type='text' name='_pihg_seed_info[Oil]' /></td>\n\
+</tr>");
+	});
+})(jQuery);
