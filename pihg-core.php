20c5e4e9020a56417c662d8815bdbfa911bd49a0
pjohanneson <pjohanneson@Lappy>
2013-12-10 22:27:04 -0600

    Add the metaboxen file back in (spluh)

--- pihg-core.php
+++ pihg-core.php
@@ -8,6 +8,7 @@
  */
 
 require_once( plugin_dir_path( __FILE__ ) . 'lib/cmb-extended/custom-meta-boxes.php' );
+require_once( plugin_dir_path( __FILE__ ) . 'lib/cmb-extended/pihg-metaboxen.php' );
 require_once( plugin_dir_path (__FILE__ ) . 'lib/helpers/helpers.php' );
 
 class PIHG {
