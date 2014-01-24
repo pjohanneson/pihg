6f4861e5aa1d2d0b1648f9fbf1bf238a7a0c0637
pjohanneson <pjohanneson@Lappy>
2014-01-23 22:05:53 -0600

    ordered seeds by title REDUX

--- pihg-core.php
+++ pihg-core.php
@@ -148,10 +148,10 @@ class PIHG {
 	function all_seeds() {
 		$all_seeds = "<div class='row'>\n";
 		$args = array(
-			'post_type' => 'pihg-seed',
-			'posts_per_page' => -1,
-			'orderby' => 'post_title',
-			'order' => 'ASC',
+			'post_type'		=> 'pihg-seed',
+			'posts_per_page'=> -1,
+			'orderby'		=> 'title',
+			'order'			=> 'ASC',
 		);
 		$seeds = new WP_Query( $args );
 		if ( $seeds->have_posts() ) {
